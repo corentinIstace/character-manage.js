64d6ca479772ea743285eea1cd505beea16e06e0
jeremilopo <jer.lopopola@gmail.com>
2021-08-12 15:14:48 +0200

    ko

--- script.js
+++ script.js
@@ -55,7 +55,7 @@
          let img = document.getElementById('test').innerHTML 
       
                           
-            const data = { heroDesc:`${heroDesc}`, shortDescription :`${heroShortDesc}`   ,name:`${heroName}` ,   image : `${img}` };
+            const data = { description:`${heroDesc}`, shortDescription :`${heroShortDesc}`   ,name:`${heroName}` ,   image : `${img}` };
             console.log(data) 
             let response = await fetch("https://character-database.becode.xyz/characters",{ 
                 method: 'POST', // or 'PUT'
@@ -68,6 +68,9 @@
             console.log(jsonans) 
      }
   })
+
+
+  
 
   
 
